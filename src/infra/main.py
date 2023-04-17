7dd8236293dd80f021a5495642658f3a15a078ca
Gabrijela Juresic <gjuresic@gmail.com>
2023-04-17 23:34:22 +0200

    changed paths

--- src/infra/main.py
+++ src/infra/main.py
@@ -4,11 +4,10 @@ from src.infra.get_geo_data import get_lat_lon_for_cities
 
 def main():
     df_cost_of_living = scrape_data_from_website()
-    df_cost_of_living.to_csv('https://github.com/gjuresic/cost_of_living_german_cities/blob/main/src/data/'
-                             'cost_of_living_data.csv')
+    df_cost_of_living.to_csv('https://raw.githubusercontent.com/gjuresic/cost_of_living_german_cities/main/src/data/cost_of_living_data.csv')
 
     df_geo_data = get_lat_lon_for_cities()
-    df_geo_data.to_csv('https://github.com/gjuresic/cost_of_living_german_cities/blob/main/src/data/geo_data.csv')
+    df_geo_data.to_csv('https://raw.githubusercontent.com/gjuresic/cost_of_living_german_cities/main/src/data/geo_data.csv')
 
 
 if __name__ == '__main__':
